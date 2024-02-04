d0a8168046ede7ab84b67afadcbc092909101de4
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2024-02-04 19:03:35 +0100

    step 1 Give the user the ability to pass a URL of some webpage.

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ const expressApp = express();
 dotenv.config();
 const PORT = process.env.PORT
 
+expressApp.get('/', (req, res) => {
+    res.send(`
+    <form = action="/findterms" method="post">
+    <label for="url">Enter URL:</label>
+    <input type="text" id="url" name="url" required/>
+    <button type="submit"> Find Terms Link</button>
+    </form>
+    `);
+})
+
 
 expressApp.listen(PORT, () => console.log(`Server listening on PORT ${PORT}.`));
